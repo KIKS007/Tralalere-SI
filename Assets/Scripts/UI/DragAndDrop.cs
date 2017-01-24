7160495162b97bdb05599c1cc31011b9df88f684
Keryann Malacchina <keryannm@gmail.com>
2017-01-24 20:25:29 +0100

    Working On UI / On Slots

--- Assets/Scripts/UI/DragAndDrop.cs
+++ Assets/Scripts/UI/DragAndDrop.cs
@@ -3,58 +3,112 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragAndDrop : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerUpHandler, IPointerDownHandler
+public class DragAndDrop : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
 {
 	public static GameObject currentItemDragged;
-	public bool isDragged = false;
 
-	private Vector3 startPosition;
-	private Transform startParent;
+	[Header ("Is Dragged")]
+	public bool isDragged = false;
 
 	private RectTransform rect;
 	private Camera mainCamera;
 
 	private float lerp = 0.2f;
 
+	private RectTransform hackingCanvas;
+
+	private float myWidth;
+	private float myHeight;
+
 	// Use this for initialization
 	void Start () 
 	{
 		rect = GetComponent<RectTransform> ();
 		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
+
+		hackingCanvas = GameObject.FindGameObjectWithTag ("HackingCanvas").GetComponent<RectTransform> ();
+
+		myWidth = (rect.rect.width + 5) / 2;
+		myHeight = (rect.rect.height + 5) / 2;
 	}
 
 	void Update ()
 	{
 		if(isDragged)
-			transform.position = Vector3.Lerp (transform.position, Input.mousePosition, lerp);
+		{
+			Vector2 pos;
+			RectTransformUtility.ScreenPointToLocalPointInRectangle(hackingCanvas.transform as RectTransform, Input.mousePosition, mainCamera, out pos);
+			transform.position = Vector3.Lerp (transform.position, hackingCanvas.transform.TransformPoint(pos * 0.6f), lerp);
 
+			BlockRect ();
+		}
+	}
+
+	void BlockRect ()
+	{
+		if(rect.anchoredPosition.x < -((hackingCanvas.rect.width / 2) - myWidth))
+		{
+			Vector3 localPos = rect.anchoredPosition;
+			localPos.x = -((hackingCanvas.rect.width / 2) - myWidth);
+			rect.anchoredPosition = localPos;
+		}
+		else if(rect.anchoredPosition.x > ((hackingCanvas.rect.width / 2) - myWidth))
+		{
+			Vector3 localPos = rect.anchoredPosition;
+			localPos.x = ((hackingCanvas.rect.width / 2) - myWidth);
+			rect.anchoredPosition = localPos;
+		}
+
+		if(rect.anchoredPosition.y < -((hackingCanvas.rect.height / 2) - myHeight) - 250)
+		{
+			Vector3 localPos = rect.anchoredPosition;
+			localPos.y = -((hackingCanvas.rect.height / 2) - myHeight) - 250;
+			rect.anchoredPosition = localPos;
+		}
+		else if(rect.anchoredPosition.y > ((hackingCanvas.rect.height / 2) - myHeight) - 250)
+		{
+			Vector3 localPos = rect.anchoredPosition;
+			localPos.y = ((hackingCanvas.rect.height / 2) - myHeight) - 250;
+			rect.anchoredPosition = localPos;
+		}
 	}
 
 	public void OnPointerDown (PointerEventData eventData)
 	{
 		isDragged = true;
 		currentItemDragged = gameObject;
-	}
 
-	public void OnBeginDrag (PointerEventData eventData)
-	{
-		
-	}
-
-	public void OnDrag (PointerEventData eventData)
-	{
-		
-	}
-
-	public void OnEndDrag (PointerEventData eventData)
-	{
-		
+		RemoveParent ();
 	}
 
 	public void OnPointerUp (PointerEventData eventData)
 	{
 		isDragged = false;
-
 		currentItemDragged = null;
+
+		CheckDrop ();
+	}
+
+	public void CheckDrop ()
+	{
+		PointerEventData pointer = new PointerEventData(EventSystem.current);
+		pointer.position = Input.mousePosition;
+
+		List<RaycastResult> raycastResults = new List<RaycastResult>();
+		EventSystem.current.RaycastAll(pointer, raycastResults);
+
+		for (int i = 0; i < raycastResults.Count; i++)
+		{
+			if(raycastResults [i].gameObject.tag == "Slot")
+			{
+				Debug.Log (raycastResults [i].gameObject);
+				raycastResults [i].gameObject.GetComponent<ScrollManager> ().AddElement (rect);
+			}
+		}
+	}
+
+	public void RemoveParent ()
+	{
+		transform.SetParent (hackingCanvas.transform);
 	}
 }
---
+++ Assets/Scripts/UI/ScrollManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ScrollManager : MonoBehaviour 
+{
+	public List<RectTransform> elements = new List<RectTransform> ();
+
+	private float _xPos = 0f;
+	private float _initialYPos = 70;
+
+	private float _duration = 0.5f;
+	private Ease _ease = Ease.OutQuad;
+
+	private float gapBetweenElements = 10f;
+
+	private RectTransform content;
+
+	public float contentHeight;
+
+	// Use this for initialization
+	void Start () 
+	{
+		content = transform.GetChild (0).GetChild (0).GetComponent<RectTransform> ();	
+
+		GetElements ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void GetElements ()
+	{
+		elements.Clear ();
+
+		for (int i = 0; i < content.transform.childCount; i++)
+		{
+			elements.Add (content.transform.GetChild (i).GetComponent<RectTransform> ());
+			//elements [i].anchoredPosition = new Vector2 (_xPos, _initialYPos);
+		}
+
+		PlaceElements ();
+	}
+
+	public void AddElement (RectTransform element)
+	{
+		
+	}
+
+	void PlaceElements ()
+	{
+		//SetContentHeight ();
+
+		for (int i = 0; i < elements.Count; i++)
+		{
+			Vector2 pos = new Vector2 (_xPos, -elements [i].sizeDelta.y * 0.5f - gapBetweenElements);
+
+			if(i == 0)
+			{
+				elements [i].DOAnchorPos (pos, _duration).SetEase (_ease);
+
+			}
+			else
+			{
+				pos = pos + new Vector2 (_xPos, -(elements [i].sizeDelta.y + gapBetweenElements) * (i));
+
+				elements [i].DOAnchorPos (pos, _duration).SetEase (_ease);	
+			}
+
+			Debug.Log (pos);
+
+		}
+
+	}
+
+	void SetContentHeight ()
+	{
+		Debug.Log ((elements [0].sizeDelta.y + gapBetweenElements) * elements.Count);
+		contentHeight = (elements [0].sizeDelta.y + gapBetweenElements) * elements.Count;
+		content.sizeDelta = new Vector2 (content.sizeDelta.x, contentHeight);
+	}
+}
